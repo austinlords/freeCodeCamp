3b3aed098d2b12e5fe5f9f6981c215557333a266
Austin Lords <austinlords@outlook.com>
2019-05-19 19:03:51 -0600

    add MongoDB and Mongoose exercises

--- javascript-algorithms-and-data-structures/index.js
+++ javascript-algorithms-and-data-structures/index.js
@@ -1,3 +1,38 @@
 // Working Script for Algorithm Solving
 
 
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function countOnline(obj) {
+  // change code below this line
+
+let count= 0;
+
+for (let user in users){
+  console.log(users[user].online);
+  if (users[user].online)
+    count++;  
+}
+return count;
+  // change code above this line
+}
+
+console.log(countOnline(users));
+
